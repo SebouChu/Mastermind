4b4018b0fa79de1bfe767904b6c16d71f4b7ba83
Sébastien Gaya <somniia.99@gmail.com>
2018-03-20 07:49:59 +0100

    Initial Commit

---
+++ Mastermind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mastermind
+//
+//  Created by Sébastien Gaya on 20/03/2018.
+//  Copyright © 2018 Sébastien Gaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
